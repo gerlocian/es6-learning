b4d33e5e0eb5bdf12679cf211a9930e89d2c1ec8
Patrick Ortiz <portiz@lulu.com>
2016-05-31 11:11:43 -0400

    Added clarification to the Raw string access examples and documentation.

--- Concepts/5_Template-Literals/3_raw-string-access.js
+++ Concepts/5_Template-Literals/3_raw-string-access.js
@@ -1,9 +1,10 @@
 /**
  * Raw string access
  *
- * I'm not really sure what this is for.
- * TODO: Do more investigation on this feature.
- * TODO: Find use cases for why this is important or what the intention is.
+ * Template literals can further be extended to allow you to manipulate a
+ * string in template form using a function. With this setup, you can take a
+ * string template and run it through a function that will do anything you
+ * want.
  */
 
 {
@@ -18,4 +19,39 @@
 
     test `Patrick Ortiz ${ value1 } Sept 21 ${ value2 }`;
     test(`Patrick Ortiz ${ value1 } Sept 21 ${ value2 }`);
+
+    // This is a bit convaluded, but I'm attempting to highlight the point.
+    function generator(strings, ...others) {
+        if (strings[0] === 'Hello ' && strings[1] === ', how are you?') {
+            return (name, lang) => {
+                const ENG_STRING = `Hello ${name}, how are you?`;
+                const SPA_STRING = `Hola ${name} , ¿cómo estás?`;
+                const GER_STRING = `Hallo ${name}, wie geht es dir?`;
+                const IRE_STRING = `Dia duit ${name} , conas atá tú ?`;
+
+                switch (lang) {
+                case 'eng':
+                    return ENG_STRING;
+                case 'spa':
+                    return SPA_STRING;
+                case 'ger':
+                    return GER_STRING;
+                case 'ire':
+                    return IRE_STRING;
+                default:
+                    return 'Invalid language';
+                }
+            };
+        } else {
+            return 'Invalid template.';
+        }
+    }
+
+    let greeting = generator `Hello ${0}, how are you?`;
+    console.log(greeting('Patrick', 'eng'));
+    console.log(greeting('Patrick', 'spa'));
+    console.log(greeting('Patrick', 'dut'));
+
+    let broken = generator `Hello world!`;
+    console.log(broken);
 }
